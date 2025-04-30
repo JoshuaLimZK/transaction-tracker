84cedef722f4c842d132d22c8a15bcb4f5cef505
Joshua Lim Zhe Kai <joshua_lim_zhe_kai@s2019.ssts.edu.sg>
2025-04-30 12:49:19 +0800

    Added header to site

--- src/app/page.js
+++ src/app/page.js
@@ -9,11 +9,12 @@ import { Suspense } from "react";
 
 export default async function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-end p-24 gap-2">
-            <div className="flex gap-2">
+        <main className="flex min-h-screen flex-col p-24 gap-2">
+            <div className="flex flex-row gap-2 w-full justify-end">
                 <FetchNewButton />
                 <ModeToggle />
             </div>
+            <h1 className=" text-3xl font-bold">Transcations</h1>
             <Suspense fallback={<LoadingTable />}>
                 <TransactionTable />
             </Suspense>
